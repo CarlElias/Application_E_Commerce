d5e065d4d4ad7feea156ccfcdf680eb88ef3f9a0
CarlElias <75279948+CarlElias@users.noreply.github.com>
2022-12-26 17:00:21 +0000

    fichier static User

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -6,6 +6,24 @@ class ItemAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('titre',)}
     list_display = ['titre', 'prix', 'prix_reduit']
 
+class ItemVariationAdmin(admin.ModelAdmin):
+    list_display = ['variation', 'valeur', 'attachment']
+    search_fields = ['valeur']
+    list_filter = ['variation', 'variation__item']
+    
+class ItemVariationInlineAdmin(admin.TabularInline):
+    model = Item_Variation
+    extra = 1
+    
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ['item', 'nom']
+    search_fields = ['item']
+    list_filter = ['item']
+    inlines = [ItemVariationInlineAdmin]
+    
+    
+admin.site.register(Item_Variation, ItemVariationAdmin)
+admin.site.register(Variation, VariationAdmin)
 admin.site.register(Item, ItemAdmin)
 admin.site.register(OrderItem)
 admin.site.register(Order)
--- catalogue/models.py
+++ catalogue/models.py
@@ -39,7 +39,31 @@ class Item(models.Model): #Item du catalogue
             'categorie': self.categorie,
             'slug': self.slug
         })
-            
+           
+class Variation(models.Model): #Variation de l'item
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    nom = models.CharField(max_length=200)
+    
+    class Meta:
+        unique_together = (
+            ('item', 'nom'),
+        )
+    
+    def __str__(self):
+        return self.nom  
+    
+class Item_Variation(models.Model): #Variation de l'item
+    variation = models.ForeignKey(Variation, on_delete=models.CASCADE)
+    valeur = models.CharField(max_length=200)
+    attachment = models.ImageField(upload_to='')
+    
+    class Meta:
+        unique_together = (
+            ('variation', 'valeur'),
+        )
+    
+    def __str__(self):
+        return self.valeur            
 
 class OrderItem(models.Model): #Item de la commande
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
